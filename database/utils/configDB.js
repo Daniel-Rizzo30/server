465bffa5903bc722502ac8cede5f006a392bf0e6
Daniel.Rizzo30 <rimshot816@DESKTOP-2UTBUI6.localdomain>
2022-05-03 18:34:33 -0400

    My first commit

--- database/utils/configDB.js
+++ database/utils/configDB.js
@@ -6,7 +6,7 @@ It declares and exports the variables for database name, username, and password.
 // Declare the variables for database name, username, and password.
 const dbName = 'starter-server';
 const dbUser = 'postgres';
-const dbPwd = '';
+const dbPwd = 'dbPwd'; // This is literally the password I put. 
 
 // Export the variables 
 module.exports = {
